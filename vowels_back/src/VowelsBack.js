17455622d0a05806ce257607b2751e525f8b5c26
mackacavs <mackenziegdixon@yahoo.com>
2019-12-05 13:46:55 +0000

    minor refactor

--- vowels_back/src/VowelsBack.js
+++ vowels_back/src/VowelsBack.js
@@ -1,15 +1,16 @@
 function vowelsBack(string) {
+  returnedString = ''
+
   mainConsonents = ['b', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q']
   consonentsPassZ = ['r', 's', 'w', 'x', 'y', 'z']
-  vowels = ['u']
   cOrO = ['c', 'o']
-  returnedString = ''
+
   for (i = 0; i < string.length; i++) {
     if (mainConsonents.includes(string[i])) {
       ascii = string[i].charCodeAt(0) + 9
     } else if (consonentsPassZ.includes(string[i])) {
       ascii = string[i].charCodeAt(0) + 9 - 26
-    } else if (vowels.includes(string[i])) {
+    } else if (string[i] === 'u') {
       ascii = string[i].charCodeAt(0) - 5
     } else if (cOrO.includes(string[i])) {
       ascii = string[i].charCodeAt(0) - 1
