f1be56ac5939a8b26a8841d483b092ea967f29bb
mackacavs <mackenziegdixon@yahoo.com>
2019-12-06 15:44:33 +0000

    bears challenge

--- bears/spec/BearsSpec.js
+++ bears/spec/BearsSpec.js
@@ -1,7 +1,13 @@
 describe("Bears", function () {
 
-  it("should equal 0", function () {
-    expect(bears()).toEqual(0);
+
+
+  it("when recieving 7 and '8j8mBliB8gimjB8B8jlB' it should return 'B8B8B8' and false", function () {
+    expect(bears(7, '8j8mBliB8gimjB8B8jlB')).toEqual(['B8B8B8', false]);
+  });
+
+  it("when recieving 3 and '88Bifk8hB8BB8BBBB888chl8BhBfd' it should return 'B8B8B8' and false", function () {
+    expect(bears(3, '88Bifk8hB8BB8BBBB888chl8BhBfd')).toEqual(['8BB8B8B88B', true]);
   });
 
 });
--- bears/src/Bears.js
+++ bears/src/Bears.js
@@ -1,3 +1,5 @@
-function bears() {
-  return 0
+function bears(x, s) {
+  var pairs = s.match(/(8B)|(B8)/g) || [];
+  return [pairs.join(""), pairs.length >= x];
+
 }
