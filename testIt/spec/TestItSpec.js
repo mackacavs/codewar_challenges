0a9002b5443e8f7518d91be1c439c93651caabab
mackacavs <mackenziegdixon@yahoo.com>
2019-12-09 11:03:48 +0000

    Have added more tests

--- testIt/spec/TestItSpec.js
+++ testIt/spec/TestItSpec.js
@@ -1,7 +1,23 @@
 describe("testIt", function () {
 
-  it("when recieving 7 and '8j8mBliB8gimjB8B8jlB' it should return 'B8B8B8' and false", function () {
+  it("when recieving [1, 2], [3, 4] it should return [1, 2, 3, 4]", function () {
     expect(testIt([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  it("when recieving [0],[1] it should return [0, 1]", function () {
+    expect(testIt([0], [1])).toEqual([0, 1]);
+  });
+
+  it("when recieving [1], [2, 3, 4] it should return [1, 2, 3, 4]", function () {
+    expect(testIt([1], [2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  it("when recieving [1, 2, 3], [4] it should return [1, 2, 3, 4]", function () {
+    expect(testIt([1, 2, 3], [4])).toEqual([1, 2, 3, 4]);
+  });
+
+  it("when recieving [1, 2], [1, 2] it should return [1, 1, 2, 2]", function () {
+    expect(testIt([1, 2], [1, 2])).toEqual([1, 1, 2, 2]);
   });
 
 });
