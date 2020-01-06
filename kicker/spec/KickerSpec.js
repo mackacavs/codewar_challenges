70cfb37fbb5de21c77af400d942e2ce8e5177c22
mackacavs <mackenziegdixon@yahoo.com>
2020-01-06 12:13:01 +0000

    added more tests

--- kicker/spec/KickerSpec.js
+++ kicker/spec/KickerSpec.js
@@ -4,4 +4,12 @@ describe("Kicker", function () {
     expect(kicker('Beautiful is better than ugly.', 10, 5)).toEqual('     Beaut');
   });
 
+  it("Should return '.         '", function () {
+    expect(kicker('Beautiful is better than ugly.', 10, 39)).toEqual('.         ');
+  });
+  it("Should return '         B'", function () {
+    expect(kicker('Beautiful is better than ugly.', 10, 41)).toEqual('         B');
+  });
+
 });
+
